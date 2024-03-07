1c4f8ee096911917b2d05d412ec6ecfa48eca165
Asmarababah <asmarababah86@gmail.com>
2024-03-08 02:23:44 +0300

    Update LoginSignup.js

--- frontend/src/Pages/LoginSignup.js
+++ frontend/src/Pages/LoginSignup.js
@@ -20,7 +20,7 @@ const LoginSignup = () => {
     const login = async () => {
         console.log("Login Function Executed", formData);
         let responseData;
-        await fetch("http://localhost:4000/login", {
+        await fetch("https://e-commerce-mern-stack-6dgz.onrender.com/login", {
             method: "POST",
             headers: {
                 Accept: "application/form-data",
@@ -40,7 +40,7 @@ const LoginSignup = () => {
     const signup = async () => {
         console.log("Sign Up Function Executed", formData);
         let responseData;
-        await fetch("http://localhost:4000/signup", {
+        await fetch("https://e-commerce-mern-stack-6dgz.onrender.com/signup", {
             method: "POST",
             headers: {
                 Accept: "application/form-data",
